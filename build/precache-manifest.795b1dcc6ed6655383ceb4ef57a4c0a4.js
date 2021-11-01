9162e17064b25e8cb1e3265765e917f7e44a9783
shakti prasad sahu <89262221+shakti-prasad-sahu@users.noreply.github.com>
2021-11-01 18:50:15 +0530

    React-Final_Assignment

---
+++ build/precache-manifest.795b1dcc6ed6655383ceb4ef57a4c0a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c62707a9ddf5edf7e185ce47dc6432",
+    "url": "/Edyoda-react-final-assignment.git/index.html"
+  },
+  {
+    "revision": "873af296bfff9d941985",
+    "url": "/Edyoda-react-final-assignment.git/static/css/main.378eeef8.chunk.css"
+  },
+  {
+    "revision": "bec6a904d6292751b104",
+    "url": "/Edyoda-react-final-assignment.git/static/js/2.cc454380.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/Edyoda-react-final-assignment.git/static/js/2.cc454380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "873af296bfff9d941985",
+    "url": "/Edyoda-react-final-assignment.git/static/js/main.b02574bc.chunk.js"
+  },
+  {
+    "revision": "7082977892a6cf02e298",
+    "url": "/Edyoda-react-final-assignment.git/static/js/runtime-main.6df27c3e.js"
+  },
+  {
+    "revision": "12f2e15784795ae4d225998722c2b6e5",
+    "url": "/Edyoda-react-final-assignment.git/static/media/avatar.12f2e157.png"
+  }
+]);
